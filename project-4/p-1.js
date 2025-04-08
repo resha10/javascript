6fbf01e1a7eef8ac5ee17a3199d6b62218a05e14
resha nakrani <reshanakrani@gmail.com>
2025-04-08 18:30:43 +0530

    8-4-2025

---
+++ project-4/p-1.js
@@ -0,0 +1,207 @@
+// 1. no return type function.
+
+ /*   let n;
+    function greet(g)
+    {
+        if(g == null)
+        {
+            console.log("good morning.......");l
+        }
+        else
+        {
+            console.log("hello");
+        }
+    }
+    greet();
+
+output:
+good morning*/
+
+
+
+// =====================================================================================================================
+
+// 2. circle area using return type with argument function.
+
+/* let pi = 3.14;
+function areacircle(r)
+{
+    areacircle = pi * r *r ;
+    console.log("area of circle",areacircle);
+}
+
+areacircle(62)
+
+output:
+
+area of circle 12070.16
+
+*/
+
+
+// =====================================================================================================================
+
+// 3. triangle area using return type with argument function.
+
+ /*   let l,h;
+    function areatriangle(l,h)
+    {
+        areatriangle =  (l * h)/2;
+        console.log("area of triangle", areatriangle);
+    }
+ areatriangle(12,17);
+
+ output:
+
+ area of triangle 102
+
+ */
+   
+// =====================================================================================================================
+
+// 4. ractangle area using return type with argument function.
+
+ /*   let len , hig;
+    function arearactangle(len , hig)
+    {
+        arearactangle = len * hig;
+        console.log("area of ractangle", arearactangle);
+    }
+arearactangle(23,14);
+
+output:
+
+area of ractangle 322
+
+*/
+
+// =====================================================================================================================
+
+// 5. find ans using return type with argument function.
+
+ /*   let a,b, c;
+    function ans(a , b , c)
+    {
+        ans = ((b *b)*(4* a * c )/(2 * a));
+        console.log("value ans",ans);
+    }
+ans(2 , 14, 7);
+
+output:
+
+value ans 2744
+
+*/
+
+// =====================================================================================================================
+
+
+// 6. find ans using return type with argument function.
+
+ /*   let a,b;
+    function ans(a , b)
+{
+    ans = (a * a) + (2 * a * b) + (b * b);
+    console.log("value ans",ans);
+}
+ans(9 , 4, 5);
+
+
+output:
+
+value ans 169
+
+*/
+
+
+// =====================================================================================================================
+
+
+// 7. celsuise to ferhinit using return type with argument function.
+
+ /*   let c;
+    function celtofeh(c)
+    {
+        celtofeh = (f =( c * 1.8)+ 32);
+        console.log("celsuise to fah", celtofeh);
+    }
+ celtofeh(12);
+
+ 
+ output:
+
+ celsuise to fah 53.6
+*/
+
+ // =====================================================================================================================
+
+// 8. ferhinit to celsuise using return type with argument function.
+
+ /*   let f;
+    function fahtocel(f)
+{
+    fahtocel = (c = (f - 32) / 1.8);
+    console.log("celsuise to fah", fahtocel);
+}
+fahtocel(97);
+
+output:
+
+celsuise to fah 36.11111111111111
+
+*/
+
+// =====================================================================================================================
+
+
+// 8. if given number odd or even using an argumnet with no return argument fubnction.
+
+ /*   function odd(num)
+    {
+        if(num == 0)
+        {
+            console.log("number is zero");
+        }
+        else if(num %2 == 0)
+        {
+            console.log("number is even....", num);
+
+        }
+        else
+        {
+            console.log("number is odd.....",num);
+        }
+    }
+
+    odd(34567);
+
+    output:
+
+    number is odd..... 34567
+
+    */
+
+
+// =====================================================================================================================
+
+// 10. swap variable with no return argument function.
+
+ /*   let a,b;
+    function swap(a,b)
+    {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        console.log(" a is.....",a);
+        console.log(" b is.....",b);
+
+    }
+swap(10,20);
+
+output:
+
+a is..... 20
+b is..... 10
+
+*/
